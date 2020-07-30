b8157a3cb81f524137c36f63349dc71b0455a20a
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-07-30 10:01:48 -0400

    install npm package

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const fs = require('fs');
 const generateMarkdown = ();
 const questionsArr = require('');
 
-inquirer.prompt(questionsArr).then(responseObj => {
+inquirer.prompt([ {
+    type:'input',
+    name:'user',
+    message:'What is your GitHub username?',
+    validate: user => {
+    if (user.length < 1 && user.length)
+      return 'User name must be valid'
+
+    }
+    else if (user.startsWith('-',0)) {
+      return 'Username can not start with numbers or special characters'  
+    }
 
 const finishedMarkdown = profileWriter.generateMarkdown(responseObj)
 
