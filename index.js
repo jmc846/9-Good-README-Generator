73cfd28f458c1ba24be2247d406c377672afc359
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-07-30 09:07:40 -0400

    updates to const and inquirer fs

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const fs = require('fs');
 const generateMarkdown = ();
 const questionsArr = require('');
 
-inquirer.prompt(questionsArr).then(responseObj)
+inquirer.prompt(questionsArr).then(responseObj => {
 
-const finishedMarkdown = generateMarkdown(responseObj)
+const finishedMarkdown = profileWriter.generateMarkdown(responseObj)
 
-fs.writeFile('./profile.md', finishedMarkdown,)
+fs.writeFile('./profile.md', finishedMarkdown, err =>{
 if(err){
     console.log(err)
 } else{
 console.log('GOOD JOB')
 
-}
+})
+})
